69c776411354794e5c3b8722fb00cd5ad9cf9357
Aleksandr Balakirev <santey3@gmail.com>
2024-03-28 10:46:12 +0100

    OpenAPI generation script updated

--- scripts/generate-openapi.sh
+++ scripts/generate-openapi.sh
@@ -7,16 +7,18 @@ cd "$(dirname "$0")"
 rm -rf .gen
 mkdir -p .gen
 cd .gen
+rm -rf ../../src/corbado_python_sdk/$GENERATED_PACKAGE_NAME
+mkdir -p ../../src/corbado_python_sdk/$GENERATED_PACKAGE_NAME
 
 curl -s -O https://api.corbado.com/docs/api/openapi/backend_api_public.yml
 docker pull openapitools/openapi-generator-cli
 docker run -v ${PWD}:/local --user $(id -u):$(id -g) openapitools/openapi-generator-cli generate -i /local/backend_api_public.yml -g python -o /local --additional-properties=packageName=$GENERATED_PACKAGE_NAME,projectName=corbado-python
 #pip install .
 
-cp -r $GENERATED_PACKAGE_NAME/* ../../src/$GENERATED_PACKAGE_NAME
-cp -r requirements.txt ../../src/$GENERATED_PACKAGE_NAME
+cp -r $GENERATED_PACKAGE_NAME/* ../../src/corbado_python_sdk/$GENERATED_PACKAGE_NAME
+cp -r requirements.txt ../../src/corbado_python_sdk/$GENERATED_PACKAGE_NAME
 
 cd ..
-rm -rf .gen
+#rm -rf .gen
 
 echo " done!"
