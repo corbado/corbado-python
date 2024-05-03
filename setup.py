4516c32bb7629667b94582b4dd0a7478ab630202
Aleksandr Balakirev <santey3@gmail.com>
2024-05-03 14:59:44 +0200

    Refactoring dependencies

--- setup.py
+++ setup.py
@@ -24,17 +24,44 @@ base_dir: str = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(base_dir, "VERSION")) as version_file:
     version: str = version_file.read().strip()
 
+CLASSIFIERS = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
+    "Programming Language :: Python :: 3.12",
+    "Operating System :: OS Independent",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
+
+
 NAME = "corbado-python-sdk"
 VERSION: str = version
 PYTHON_REQUIRES = ">=3.8"
-REQUIRES = [
-    "urllib3 >= 1.25.3, < 2.1.0",
-    "python-dateutil",
+INSTALL_REQUIRES = [
+    "urllib3 >= 1.25.3",
+    "python-dateutil",  # used in generated code
     "pydantic >= 2",
     "typing-extensions >= 4.7.1",
     "pyopenssl",
     "PyJWT",
     "flask",
+]
+gh_requires = ["tox-gh>=1.2"]
+flake8_requires = [
+    "flake8",
+    "pep8-naming",  # naming conventions check
+    "flake8-builtins",
+    "flake8-bugbear",
+    "flake8-isort",  # check if import is sorted
+    "darglint",
+    "flake8-bandit",  # security check
+    "flake8-return",
+    "flake8-docstring-checker",  # enforce docstring
 ]
 
 setup(
@@ -45,10 +72,11 @@ setup(
     author_email="support@corbado.com",
     url="https://github.com/corbado/corbado-python",
     keywords=["Corbado", "Corbado Python SDK"],
-    install_requires=REQUIRES,
+    install_requires=INSTALL_REQUIRES,
     packages=find_namespace_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,
+    classifiers=CLASSIFIERS,
     long_description_content_type="text/markdown",
     long_description=open(os.path.join(base_dir, "README.md"), encoding="utf-8").read(),
     package_data={"corbado_sdk": ["py.typed"]},
