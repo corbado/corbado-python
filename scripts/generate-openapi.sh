3ee7a7648b40ae9efd843e38ce67565f07b45100
Dopeamin <mohamed.amine.hamdouni@corbado.com>
2025-06-12 13:50:49 +0200

    Refactor backend API specification by consolidating definitions from common.yml into backend_api_public_v2.yml and updating the OpenAPI paths. Remove common.yml as it is no longer needed. Update generate-openapi.sh to fetch the latest backend API specification directly from the server.

--- scripts/generate-openapi.sh
+++ scripts/generate-openapi.sh
@@ -6,7 +6,6 @@ echo "Generating OpenAPI code ..."
 
 cd "$(dirname "$0")"
 cp backend_api_public_v2.yml .gen/backend_api_public_v2.yml
-cp common.yml .gen/common.yml
 #rm -rf .gen
 mkdir -p .gen
 cd .gen
@@ -14,8 +13,7 @@ rm -rf ../../src/$SDK_PACKAGE_NAME/$GENERATED_PACKAGE_NAME
 mkdir -p ../../src/$SDK_PACKAGE_NAME/$GENERATED_PACKAGE_NAME
 
 
-#curl -s -O https://backendapi.cloud.corbado.io/v2/openapi.yaml use local files until the API specification is uploaded.
-docker pull openapitools/openapi-generator-cli
+curl -s https://backendapi.cloud.corbado.io/v2/openapi.yaml -o backend_api_public_v2.yml
 docker run -v ${PWD}:/local --user $(id -u):$(id -g) openapitools/openapi-generator-cli generate -i /local/backend_api_public_v2.yml -g python -o /local --additional-properties=packageName=$SDK_PACKAGE_NAME.$GENERATED_PACKAGE_NAME,projectName=$GENERATED_PROJECT_NAME
 
 cp -r $SDK_PACKAGE_NAME/$GENERATED_PACKAGE_NAME/* ../../src/$SDK_PACKAGE_NAME/$GENERATED_PACKAGE_NAME
